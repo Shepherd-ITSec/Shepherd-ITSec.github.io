05c233048b0d8f020d7e64e904025f0f6fb1a41c
Shepherd-ITSec <felix@Shepherd-ITSec.com>
2025-12-29 22:06:56 +0100

    fix popup

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -29,20 +29,20 @@ const routes: RouteRecordRaw[] = [
         component: () => import('../pages/BusinessCardPage.vue'),
         meta: { title: 'Business Card' }
       },
-      {
-        path: 'card/popup',
-        component: () => import('../layouts/PopupLayout.vue'),
-        children: [
-          {
-            path: '',
-            component: () => import('../pages/BusinessCardPage.vue'),
-            meta: { title: 'Business Card' }
-          }
-        ]
-      },
       { path: 'posts', component: () => import('../pages/PostsIndexPage.vue'), meta: { title: 'Posts' } },
       postRoute,
       { path: ':pathMatch(.*)*', component: () => import('../pages/NotFoundPage.vue'), meta: { title: '404 Page not found' } }
+    ]
+  },
+  {
+    path: '/card/popup',
+    component: () => import('../layouts/PopupLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../pages/BusinessCardPage.vue'),
+        meta: { title: 'Business Card' }
+      }
     ]
   }
 ]
